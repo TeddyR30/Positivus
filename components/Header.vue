0738006f399a29b723f2af89d0d074fd704a483f
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-07-10 22:20:15 +0700

    Pertama

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>Header Komponen</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+header {
+    background: #42b983;
+    padding: 20px;
+    color: white;
+    text-align: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>OKE GAESSSSS</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #35495e;
+}
+</style>
