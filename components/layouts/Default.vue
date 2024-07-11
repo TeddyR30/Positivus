1a1deface30ff43ec39512b09b85c21fe5cc1736
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-07-11 16:52:18 +0700

    Ketiga

---
+++ components/layouts/Default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header />
+        <Nuxt />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '~/components/layouts/Header.vue';
+import Footer from '~/components/layouts/Footer.vue';
+
+export default {
+    components: {
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
